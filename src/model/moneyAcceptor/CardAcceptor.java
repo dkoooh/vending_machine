89a55fab2f05b49733d10502f37d6881cd9c9e00
Danis Kurenkiev <kurenkievdd@gmail.com>
2023-12-07 12:28:26 +0600

    Wrote the logic for entering a card number and one-time password in CardAcceptor

--- src/model/moneyAcceptor/CardAcceptor.java
+++ src/model/moneyAcceptor/CardAcceptor.java
@@ -1,8 +1,16 @@
 package model.moneyAcceptor;
 
+import java.util.Scanner;
+
 public class CardAcceptor implements MoneyAcceptor{
+    private final Scanner sc = new Scanner (System.in);
+
     private int amount;
+    private String oneTimePassword;
+    private final Long cardNumber;
     public CardAcceptor(int amount) {
+        cardNumber = enterCardNumber();
+        oneTimePassword = enterPassword();
         this.amount = amount;
     }
 
@@ -14,5 +22,33 @@ public class CardAcceptor implements MoneyAcceptor{
     @Override
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    private Long enterCardNumber() {
+        while (true) {
+            System.out.print("Введите номер карты (без пробелов): ");
+            String strCardNumber = sc.nextLine().trim();
+            if (!strCardNumber.isEmpty()) {
+                if (strCardNumber.length() == 16) {
+                    return Long.parseLong(strCardNumber);
+                } else {
+                    throw new IllegalArgumentException("Номер карты должен состоять из 16 символов..");
+                }
+            } else {
+                throw new IllegalArgumentException("Вы не ввели номер карты..");
+            }
+        }
+    }
+
+    private String enterPassword () {
+        while (true) {
+            System.out.print("Введите одноразовый код: ");
+            String oneTimePassword = sc.nextLine().trim();
+            if (!oneTimePassword.isEmpty()) {
+                return oneTimePassword;
+            } else {
+                throw new IllegalArgumentException("Вы не ввели код..");
+            }
+        }
     }
 }
